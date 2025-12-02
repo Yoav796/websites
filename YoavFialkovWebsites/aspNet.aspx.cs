7b3608235bc47d02d5c8d4ff00ab9f6a1c27449d
noa <noasagi88@gmail.com>
2025-12-02 16:53:47 +0200

    לוח הכפל

---
+++ YoavFialkovWebsites/aspNet.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class aspx : System.Web.UI.Page
+{
+    public string strkefel;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        strkefel = "<table border='1' width = '80%' ; height = '500px'>";
+        for (int i = 1; i <= 10; i++)
+        {
+            strkefel += "<tr>";
+            for (int j = 1; j <= 10; j++)
+            {
+                strkefel += "<td>";
+                strkefel += i * j;
+                strkefel += "</td>";
+            } 
+            strkefel += "</tr>";
+        }
+        strkefel += "</table>";
+
+    }
+}
